ee8152bf9bd8cfb1905fc86958837e7e70df57fb
rst76 <ryosuke.takashima@gmail.com>
2012-12-04 23:31:11 +0900

    Slightly changed the functions around hello_world.

--- LazyK/Golf.hs
+++ LazyK/Golf.hs
@@ -9,16 +9,16 @@ v = K :$ (K :$ num 118)
 up = K :$ (delete (V "a") (V "a" :$ (S :$ numB 2 :$ S :$ b) :$ K :$ chr '2'))
 
 -- With the least definitions.
-hello0 = K :$ foldr (consXY . num' . fromEnum) (K :$ num' 256) "Hello, world!"
+hello0 = K :$ (foldr (consXY . num' . fromEnum) (K :$ num' 256) "Hello, world!")
   where
   num' 1 = I
   num' n = S :$ (S :$ (K :$ S) :$ K) :$ num' (n - 1)
 
 -- With shorter Church Numerals (by 51b).
-hello1 = K :$ foldr (consXY . chr) endOfOutput "Hello, world!"
+hello1 = K :$ (foldr (consXY . chr) endOfOutput "Hello, world!")
 
 -- Introduced recursion.
-hello2 = K :$ foldl (\e c -> e :$ (numB (fromEnum c))) (m :$ hh :$ endOfOutput) "!dlrow ,olleH\0"
+hello2 = K :$ (foldl (\e c -> e :$ (numB (fromEnum c))) (m :$ hh :$ endOfOutput) "!dlrow ,olleH\0")
   where
   hh = delete (V "f") $ delete (V "x") $ delete (V "y") $
     ifnozeroXYZ (V "y" :$ b)
@@ -26,7 +26,7 @@ hello2 = K :$ foldl (\e c -> e :$ (numB (fromEnum c))) (m :$ hh :$ endOfOutput) 
       (V "x")
 
 -- Defined N as <27> + f <73> <81>.
-hello = K :$ foldl (\e c -> e :$ (nn (fromEnum c))) (m :$ hh :$ K) "!dlrow ,olleH\0"
+hello = K :$ (foldl (\e c -> e :$ (nn (fromEnum c))) (m :$ hh :$ K) "!dlrow ,olleH\0")
   where
   hh = delete (V "f") $ delete (V "x") $ delete (V "y") $
     ifnozeroXYZ (V "y" :$ numB 1 :$ numB 1 :$ b)
@@ -46,7 +46,7 @@ hello = K :$ foldl (\e c -> e :$ (nn (fromEnum c))) (m :$ hh :$ K) "!dlrow ,olle
   nn 114 = K :$ (S :$ (S :$ numB 6 :$ S))
   nn 119 = K :$ (S :$ (S :$ numB 11 :$ S))
 
--- For code golf with restriction on the kind of characters / code golf of iota.
+-- For code golf with restriction on the kind of characters.
 hello_sk = replace I (S :$ K :$ K) $ hello
 
 -- main = putStr $ showG v
